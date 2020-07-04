8da245e20ea2f0639bb8727e8284f938545ed3ff
Brett Evrist <brettevrist10@gmail.com>
2020-07-04 04:17:12 +0000

    frontend chart updates on load

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -1,38 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
+func showFrontend(w http.ResponseWriter, r *http.Request) {
+	// if r.URL.Path != "/" {
+	// 	http.Error(w, "404 not found.", http.StatusNotFound)
+	// 	return
+	// }
 
-	// log.Println(r.URL.String()) TODO: change form target to match the URL string
+	// log.Println(r.URL.String()) //TODO: change form target to match the URL string
 
 	if r.Method == "POST" {
-		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-		weight := r.FormValue("weight")
-		height := r.FormValue("height")
-		waist := r.FormValue("waist")
-		fmt.Fprintf(w, "weight = %s\n", weight)
-		fmt.Fprintf(w, "height = %s\n", height)
-		fmt.Fprintf(w, "waist = %s\n", waist)
-	} else {
+		weight, _ := strconv.Atoi(r.FormValue("weight"))
+		height, _ := strconv.Atoi(r.FormValue("height"))
+		waist, _ := strconv.Atoi(r.FormValue("waist"))
+		values := map[string]int{"weight": weight, "height": height, "waist": waist}
+		jsonValue, _ := json.Marshal(values)
+		_, _ = http.Post("http://example.com", "application/json", bytes.NewBuffer(jsonValue))
+		// log.Println(string(jsonValue))
+
 		http.ServeFile(w, r, "index.html")
+	} else if r.Method == "GET" {
+		http.ServeFile(w, r, "index.html")
+	} else {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
 	}
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", showFrontend)
 
 	fmt.Printf("listening at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
