4bc3950c89c00c1b04af1106619b684026ee4541
Brett Evrist <brettevrist10@gmail.com>
2020-07-03 23:42:00 -0700

    made frontend and backend work together

--- backend/backend.go
+++ backend/backend.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -18,23 +20,38 @@ type Client struct {
 
 type ClientList []Client
 
-// Inputting info into the array and encoding it into the json format
+// Clients handles user input requests and responses from database
 func Clients(w http.ResponseWriter, r *http.Request) {
-	clients := ClientList{
-		Client{FirstName: "Anthony", LastName: "Hanna", Weight: 215, WaistCirc: 33, HeightInches: 75, LeanBodyMass: 180},
-		Client{FirstName: "Ray", LastName: "Hanna", Weight: 202, WaistCirc: 29, HeightInches: 68, LeanBodyMass: 159},
+	//handling POST data upload
+	if r.Method == "POST" {
+		// unmarshal the body of POST request as a Client struct
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		var client Client
+		json.Unmarshal(reqBody, &client)
+
+		//TODO move data into backend database
+
+		fmt.Fprintf(w, "SUCCESS")
+		out, _ := json.Marshal(client)
+		log.Printf("%v\n", string(out))
+	} else if r.Method == "GET" { //respond to GET requests
+		clients := ClientList{
+			Client{FirstName: "Anthony", LastName: "Hanna", Weight: 215, WaistCirc: 33, HeightInches: 75, LeanBodyMass: 180},
+			Client{FirstName: "Ray", LastName: "Hanna", Weight: 202, WaistCirc: 29, HeightInches: 68, LeanBodyMass: 159},
+		}
+
+		json.NewEncoder(w).Encode(clients)
+		// TODO Make a template function to local and populate the html file
+		//TODO load data from a database
+		// fmt.Fprintf(w, "Backend Ready")
 	}
-
-	json.NewEncoder(w).Encode(clients)
-	// TODO Make a template function to local and populate the html file
-}
-
-func handleRequests() {
-	http.HandleFunc("/", Clients)
-	log.Fatal(http.ListenAndServe(":80", nil))
-
 }
 
 func main() {
-	handleRequests()
+	http.HandleFunc("/", Clients)
+
+	log.Printf("Backend listening at address localhost:8090\n")
+	if err := http.ListenAndServe(":8090", nil); err != nil {
+		log.Fatal(err)
+	}
 }
--- frontend/frontend.go
+++ frontend/frontend.go
@@ -3,36 +3,44 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-func showFrontend(w http.ResponseWriter, r *http.Request) {
-	// if r.URL.Path != "/" {
-	// 	http.Error(w, "404 not found.", http.StatusNotFound)
-	// 	return
-	// }
+var backendURL string //global variable for backend server address
 
-	// log.Println(r.URL.String()) //TODO: change form target to match the URL string
-
-	if r.Method == "POST" {
+// ShowFrontend respond to get requests with webpage, forward POST form to backend
+func ShowFrontend(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" { //handle form post and forward responses to backend
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
+		//extract form data
 		weight, _ := strconv.Atoi(r.FormValue("weight"))
 		height, _ := strconv.Atoi(r.FormValue("height"))
 		waist, _ := strconv.Atoi(r.FormValue("waist"))
-		values := map[string]int{"weight": weight, "height": height, "waist": waist}
+		//format form data in JSON
+		values := map[string]int{"Weight": weight, "HeightInches": height, "WaistCirc": waist}
 		jsonValue, _ := json.Marshal(values)
-		_, _ = http.Post("http://example.com", "application/json", bytes.NewBuffer(jsonValue))
-		// log.Println(string(jsonValue))
+
+		//forward form responses to backend server
+		response, _ := http.Post(backendURL, "application/json", bytes.NewBuffer(jsonValue))
+		responseBody, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("POST:" + string(jsonValue))
+		log.Println("Backend Response: " + string(responseBody))
 
 		http.ServeFile(w, r, "index.html")
-	} else if r.Method == "GET" {
-		http.ServeFile(w, r, "index.html")
+	} else if r.Method == "GET" { //serve get request
+		http.ServeFile(w, r, "index.html") //TODO use templates to have javascript pull from proper backend
 	} else {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -40,9 +48,21 @@ func showFrontend(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", showFrontend)
+	//take in command line flags
+	backendURL = *flag.String("b", "http://localhost:8090", "URL of backend endpoint")
+	flag.Parse()
 
-	fmt.Printf("listening at port 8080\n")
+	//check for backend server avalibility
+	log.Println("Checking for backend server at " + backendURL)
+	backendResponse, err := http.Get(backendURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("BackendStatus:" + backendResponse.Status)
+
+	//start server
+	http.HandleFunc("/", ShowFrontend)
+	log.Println("Frontend listening at address localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
