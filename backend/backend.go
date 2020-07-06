babcf972b91b947871e2c793678173947dbc793e
Brett Evrist <brettevrist10@gmail.com>
2020-07-06 01:18:41 +0000

    add cmd flags and handle errors on backend

--- backend/backend.go
+++ backend/backend.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+// Global Variables
+var listenAddress = flag.String("l", ":8090", "Webserver listen address") //holds http server listen address
 
 // Declaring Struct for user information. This will be pulled from the database I think.
 type Client struct {
@@ -25,13 +29,22 @@ func Clients(w http.ResponseWriter, r *http.Request) {
 	//handling POST data upload
 	if r.Method == "POST" {
 		// unmarshal the body of POST request as a Client struct
-		reqBody, _ := ioutil.ReadAll(r.Body)
+		reqBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 		var client Client
-		json.Unmarshal(reqBody, &client)
+		err = json.Unmarshal(reqBody, &client)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		//TODO move data into backend database
+		//TODO move data in client struct into backend database
 
+		//respond to POST with server status
 		fmt.Fprintf(w, "SUCCESS")
+
+		//print received data to output //FIXME remove
 		out, _ := json.Marshal(client)
 		log.Printf("%v\n", string(out))
 	} else if r.Method == "GET" { //respond to GET requests
@@ -40,18 +53,22 @@ func Clients(w http.ResponseWriter, r *http.Request) {
 			Client{FirstName: "Ray", LastName: "Hanna", Weight: 202, WaistCirc: 29, HeightInches: 68, LeanBodyMass: 159},
 		}
 
+		//respond with JSON object
 		json.NewEncoder(w).Encode(clients)
-		// TODO Make a template function to local and populate the html file
+
+		//TODO Make a template function to local and populate the html file
 		//TODO load data from a database
-		// fmt.Fprintf(w, "Backend Ready")
 	}
 }
 
 func main() {
-	http.HandleFunc("/", Clients)
+	//parse command line flags (declared with global variables)
+	flag.Parse()
 
-	log.Printf("Backend listening at address localhost:8090\n")
-	if err := http.ListenAndServe(":8090", nil); err != nil {
+	//start http server
+	http.HandleFunc("/", Clients)
+	log.Printf("Backend listening at address " + *listenAddress)
+	if err := http.ListenAndServe(*listenAddress, nil); err != nil {
 		log.Fatal(err)
 	}
 }
