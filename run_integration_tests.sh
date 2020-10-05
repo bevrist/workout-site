34102421b3368506d30a13b669c3b49787b758bf
Brett Evrist <brettevrist10@gmail.com>
2020-10-05 03:36:21 +0000

    speed up tests and terminate hanging frontend-service

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -5,16 +5,16 @@ set -e
 
 # --- build all containers ---
 # service containers
-docker build -t auth:latest -f ./auth/Dockerfile .
-docker build -t database:latest -f ./database/Dockerfile .
-docker build -t backend:latest -f ./backend/Dockerfile .
-docker build -t frontend-api:latest -f ./frontend-api/Dockerfile .
+docker build -t auth:$(git rev-parse --short HEAD) -f ./auth/Dockerfile .
+docker build -t database:$(git rev-parse --short HEAD) -f ./database/Dockerfile .
+docker build -t backend:$(git rev-parse --short HEAD) -f ./backend/Dockerfile .
+docker build -t frontend-api:$(git rev-parse --short HEAD) -f ./frontend-api/Dockerfile .
 # testing containers
-docker build -t auth-test:latest -f ./auth/tests.Dockerfile .
-docker build -t database-test:latest -f ./database/tests.Dockerfile .
-docker build -t mongodb-mock-database:latest -f ./database/mongoDB/Dockerfile .
-# docker build -t backend-test:latest -f ./backend/tests.Dockerfile .
-docker build -t frontend-api-test:latest -f ./frontend-api/tests.Dockerfile .
+docker build -t auth-test:$(git rev-parse --short HEAD) -f ./auth/tests.Dockerfile .
+docker build -t database-test:$(git rev-parse --short HEAD) -f ./database/tests.Dockerfile .
+docker build -t mongodb-mock-database:$(git rev-parse --short HEAD) -f ./database/mongoDB/Dockerfile .
+# docker build -t backend-test:$(git rev-parse --short HEAD) -f ./backend/tests.Dockerfile .
+docker build -t frontend-api-test:$(git rev-parse --short HEAD) -f ./frontend-api/tests.Dockerfile .
 
 
 # --- run all tests ---
@@ -24,11 +24,12 @@ echo "running tests..."
 echo "cleaning up hanging containers..."
 docker stop mongodb-mock-database || :
 docker stop database-service || :
+sleep 5
 echo "preparing database integration test..."
-docker run --rm -d --name=mongodb-mock-database -p 27017:27017 --net=host -e MONGO_INITDB_ROOT_USERNAME=adminz -e MONGO_INITDB_ROOT_PASSWORD=cheeksbutt mongodb-mock-database:latest
-sleep 2 && docker run -i --rm --name=database-service -p 8050:8050 --net=host database:latest &
+docker run --rm -d --name=mongodb-mock-database -p 27017:27017 --net=host -e MONGO_INITDB_ROOT_USERNAME=adminz -e MONGO_INITDB_ROOT_PASSWORD=cheeksbutt mongodb-mock-database:$(git rev-parse --short HEAD)
+sleep 5 && docker run -i --rm --name=database-service -p 8050:8050 --net=host database:$(git rev-parse --short HEAD) &
 echo "testing database..."
-sleep 2 && docker run --rm -i --name=database-test --net=host database-test:latest
+sleep 5 && docker run --rm -i --name=database-test --net=host database-test:$(git rev-parse --short HEAD)
 echo "cleaning up..."
 docker stop mongodb-mock-database
 docker stop database-service
@@ -37,10 +38,11 @@ echo ""
 # auth test
 echo "cleaning up hanging containers..."
 docker stop auth-service || :
+sleep 5
 echo "preparing auth integration test..."
-docker run -i --name=auth-service -p 8070:8070 --net=host --rm -e AUTH_FIREBASE_CREDENTIALS='{test}' auth:latest &
+docker run -i --name=auth-service -p 8070:8070 --net=host --rm -e AUTH_FIREBASE_CREDENTIALS='{test}' auth:$(git rev-parse --short HEAD) &
 echo "testing auth..."
-sleep 2 && docker run --rm -i --name=auth-test --net=host auth-test:latest
+sleep 5 && docker run --rm -i --name=auth-test --net=host auth-test:$(git rev-parse --short HEAD)
 echo "cleaning up..."
 docker stop auth-service
 echo ""
@@ -54,15 +56,18 @@ docker stop auth-service || :
 docker stop mongodb-mock-database || :
 docker stop database-service || :
 docker stop backend-service || :
+docker stop frontend-api-service || :
+sleep 5
 echo "preparing frontend-api integration test..."
-docker run -i --name=auth-service -p 8070:8070 --net=host --rm -e AUTH_FIREBASE_CREDENTIALS='{test}' auth:latest &
-docker run --rm -d --name=mongodb-mock-database -p 27017:27017 --net=host -e MONGO_INITDB_ROOT_USERNAME=adminz -e MONGO_INITDB_ROOT_PASSWORD=cheeksbutt mongodb-mock-database:latest
-sleep 2 && docker run -d --rm --name=database-service -p 8050:8050 --net=host database:latest
-sleep 2 && docker run -d --rm --name=backend-service -p 8050:8050 --net=host backend:latest
-sleep 2 && docker run -i --rm --name=frontend-api-service -p 8888:8888 --net=host frontend-api:latest &
+docker run -i --name=auth-service -p 8070:8070 --net=host --rm -e AUTH_FIREBASE_CREDENTIALS='{test}' auth:$(git rev-parse --short HEAD) &
+docker run --rm -d --name=mongodb-mock-database -p 27017:27017 --net=host -e MONGO_INITDB_ROOT_USERNAME=adminz -e MONGO_INITDB_ROOT_PASSWORD=cheeksbutt mongodb-mock-database:$(git rev-parse --short HEAD)
+sleep 5 && docker run -d --rm --name=database-service -p 8050:8050 --net=host database:$(git rev-parse --short HEAD)
+sleep 5 && docker run -d --rm --name=backend-service -p 8050:8050 --net=host backend:$(git rev-parse --short HEAD)
+sleep 5 && docker run -i --rm --name=frontend-api-service -p 8888:8888 --net=host frontend-api:$(git rev-parse --short HEAD) &
 echo "testing frontend-api..."
-sleep 2 && docker run --rm -i --name=frontend-api-test --net=host frontend-api-test:latest
+sleep 5 && docker run --rm -i --name=frontend-api-test --net=host frontend-api-test:$(git rev-parse --short HEAD)
 echo "cleaning up..."
+docker stop frontend-api-service
 docker stop auth-service
 docker stop backend-service
 docker stop database-service
